edf85baf50802d84ace7f06170867a72f2cf75fd
MrGoose <kanes137@gmail.com>
2021-08-10 20:47:51 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ru/test/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/test/sandbox/MyFirstProgram.java
@@ -1,7 +1,28 @@
 package ru.test.sandbox;
 
 public class MyFirstProgram {
-	public static void main(String[] args){
-		System.out.println("Hello, world");
-	}
+  public static void main(String[] args) {
+    hello("world");
+    hello("user");
+    hello("MrGoose");
+
+    int l = 5;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static int area(int l) {
+    return l * l;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
+  }
 }
