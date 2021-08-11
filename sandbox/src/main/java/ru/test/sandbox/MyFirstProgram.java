195f28058b894653e91830cb212b54e14397d334
MrGoose <kanes137@gmail.com>
2021-08-11 10:16:06 +0300

    Домашнее задание №2 (лекции 1.1-1.9)

--- sandbox/src/main/java/ru/test/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/test/sandbox/MyFirstProgram.java
@@ -17,6 +17,8 @@ public class MyFirstProgram {
     Point p1 = new Point(-10,-2);
     Point p2 = new Point(1,1);
     System.out.println("Расстояние между двумя точками " + p1.x + ";" + p1.y + " и " + p2.x + ";" + p2.y + " = " + distance(p1,p2));
+
+    System.out.println("Расстояние между двумя точками " + p1.x + ";" + p1.y + " и " + p2.x + ";" + p2.y + " = " + p2.distance(p1,p2));
   }
 
   public static void hello(String somebody) {
