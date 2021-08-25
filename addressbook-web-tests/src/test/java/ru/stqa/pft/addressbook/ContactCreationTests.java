c5a72d2e5b8a414dc09b97df798b691fb28bc09f
MrGoose <kanes137@gmail.com>
2021-08-25 12:27:12 +0300

    Задание №4 Сгруппировал параметры заполнения формы в объект ContactData

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -26,7 +26,7 @@ public class ContactCreationTests {
   @Test
   public void testContactCreationTests() throws Exception {
     initContactCreation();
-    fillContactForm("Goose", "Alekseevich", "Mr", "kanes173");
+    fillContactForm(new ContactData("Goose", "Alekseevich", "Mr", "kanes173"));
     saveContact();
     goToHomePage();
     logout();
@@ -40,17 +40,17 @@ public class ContactCreationTests {
     driver.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
   }
 
-  private void fillContactForm(String firstname, String middlename, String lastname, String nickname) {
+  private void fillContactForm(ContactData contactData) {
     driver.findElement(By.name("firstname")).click();
     driver.findElement(By.name("firstname")).clear();
-    driver.findElement(By.name("firstname")).sendKeys(firstname);
+    driver.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
     driver.findElement(By.name("middlename")).clear();
-    driver.findElement(By.name("middlename")).sendKeys(middlename);
+    driver.findElement(By.name("middlename")).sendKeys(contactData.getMiddlename());
     driver.findElement(By.name("lastname")).clear();
-    driver.findElement(By.name("lastname")).sendKeys(lastname);
+    driver.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
     driver.findElement(By.name("nickname")).click();
     driver.findElement(By.name("nickname")).clear();
-    driver.findElement(By.name("nickname")).sendKeys(nickname);
+    driver.findElement(By.name("nickname")).sendKeys(contactData.getNickname());
   }
 
   private void initContactCreation() {
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,31 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+  private final String firstname;
+  private final String middlename;
+  private final String lastname;
+  private final String nickname;
+
+  public ContactData(String firstname, String middlename, String lastname, String nickname) {
+    this.firstname = firstname;
+    this.middlename = middlename;
+    this.lastname = lastname;
+    this.nickname = nickname;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getMiddlename() {
+    return middlename;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getNickname() {
+    return nickname;
+  }
+}
