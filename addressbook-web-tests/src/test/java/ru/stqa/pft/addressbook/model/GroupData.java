da28ac677f08fa4d0e8293aedbfc77120de7486a
MrGoose <kanes137@gmail.com>
2021-09-13 21:13:57 +0300

    5.5. Использование уникальных идентификаторов объектов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -50,18 +50,21 @@ public class GroupData {
             '}';
   }
 
-  @Override
+  @Override //Метод сравнениия
   public boolean equals(Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
 
     GroupData groupData = (GroupData) o;
 
+    if (id != groupData.id) return false;
     return name != null ? name.equals(groupData.name) : groupData.name == null;
   }
 
   @Override
   public int hashCode() {
-    return name != null ? name.hashCode() : 0;
+    int result = id;
+    result = 31 * result + (name != null ? name.hashCode() : 0);
+    return result;
   }
 }
